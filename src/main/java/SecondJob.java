d9c122b8fb5a8196851cf738b6190d73810f9628
mohamedbahloul <bahloulmohamedd@gmail.com>
2023-12-16 14:31:21 +0100

    add "granularité"

--- src/main/java/SecondJob.java
+++ src/main/java/SecondJob.java
@@ -1,4 +1,7 @@
 import java.io.IOException;
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
+import java.time.temporal.WeekFields;
 import java.util.Arrays;
 import java.util.HashSet;
 import java.util.Set;
@@ -17,15 +20,69 @@ import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 import org.apache.hadoop.mapreduce.lib.output.TextOutputFormat;
 
 public class SecondJob {
-    public static class StatsMapper
+    public static class StatsMapperAll
             extends Mapper<Game, NullWritable, Text, GameSummary> {
 
         @Override
         public void map(Game key, NullWritable value, Context context) throws IOException, InterruptedException {
+
             context.write(new Text(key.getCards().toString()),
-                    new GameSummary(key.getWin() ? 1 : 0, 1, key.getClanTr(), key.getDeck() - key.getDeck2(), key.getPlayer()));
-            context.write(new Text(key.getCards2().toString()), new GameSummary(key.getWin() ? 0 : 1, 1, key.getClanTr2(),
-                    key.getDeck2() - key.getDeck(), key.getPlayer2()));
+                    new GameSummary(key.getWin() ? 1 : 0, 1, key.getClanTr(), key.getDeck() - key.getDeck2(),
+                            key.getPlayer()));
+
+            context.write(new Text(key.getCards2().toString()),
+                    new GameSummary(key.getWin() ? 0 : 1, 1, key.getClanTr2(), key.getDeck2() - key.getDeck(),
+                            key.getPlayer2()));
+
+        }
+    }
+
+    public static class StatsMapperWeek
+            extends Mapper<Game, NullWritable, Text, GameSummary> {
+
+        @Override
+        public void map(Game key, NullWritable value, Context context) throws IOException, InterruptedException {
+            ZonedDateTime zonedDateTime = ZonedDateTime.ofInstant(key.getDate(), ZoneId.systemDefault());
+
+            int year = zonedDateTime.getYear();
+            int week = zonedDateTime.get(WeekFields.ISO.weekOfWeekBasedYear());
+
+            context.write(new Text(key.getCards().toString() + "_w_" + week + "_y_" +
+                    year),
+                    new GameSummary(key.getWin() ? 1 : 0, 1, key.getClanTr(), key.getDeck() -
+                            key.getDeck2(),
+                            key.getPlayer()));
+
+            context.write(new Text(key.getCards2().toString() + "_w_" + week + "_y_" +
+                    year),
+                    new GameSummary(key.getWin() ? 0 : 1, 1, key.getClanTr2(), key.getDeck2() -
+                            key.getDeck(),
+                            key.getPlayer2()));
+        }
+    }
+
+    public static class StatsMapperMonth
+            extends Mapper<Game, NullWritable, Text, GameSummary> {
+
+        @Override
+        public void map(Game key, NullWritable value, Context context) throws IOException, InterruptedException {
+            ZonedDateTime zonedDateTime = ZonedDateTime.ofInstant(key.getDate(), ZoneId.systemDefault());
+
+            // Extrait le mois, l'année et la semaine
+            int month = zonedDateTime.getMonthValue();
+            int year = zonedDateTime.getYear();
+
+            context.write(new Text(key.getCards().toString() + "_m_" + month + "_y_" +
+                    year),
+                    new GameSummary(key.getWin() ? 1 : 0, 1, key.getClanTr(), key.getDeck() -
+                            key.getDeck2(),
+                            key.getPlayer()));
+
+            context.write(new Text(key.getCards2().toString() + "_m_" + month + "_y_" +
+                    year),
+                    new GameSummary(key.getWin() ? 0 : 1, 1, key.getClanTr2(), key.getDeck2() -
+                            key.getDeck(),
+                            key.getPlayer2()));
         }
     }
 
@@ -79,10 +136,10 @@ public class SecondJob {
 
     public static void main(String[] args) throws Exception {
         Configuration conf = new Configuration();
-        Job job2 = Job.getInstance(conf, "SecondJob");
+        Job job2 = Job.getInstance(conf, "SecondJobAll");
         job2.setNumReduceTasks(1);
         job2.setJarByClass(SecondJob.class);
-        job2.setMapperClass(StatsMapper.class);
+        job2.setMapperClass(StatsMapperAll.class);
         job2.setCombinerClass(StatsCombiner.class);
         job2.setMapOutputKeyClass(Text.class);
         job2.setMapOutputValueClass(GameSummary.class);
@@ -93,6 +150,41 @@ public class SecondJob {
         job2.setInputFormatClass(SequenceFileInputFormat.class);
         FileInputFormat.addInputPath(job2, new Path(args[0]));
         FileOutputFormat.setOutputPath(job2, new Path(args[1]));
-        System.exit(job2.waitForCompletion(true) ? 0 : 1);
+
+        Configuration confweek = new Configuration();
+        Job jobweek = Job.getInstance(confweek, "SecondJobWeek");
+        jobweek.setNumReduceTasks(1);
+        jobweek.setJarByClass(SecondJob.class);
+        jobweek.setMapperClass(StatsMapperWeek.class);
+        jobweek.setCombinerClass(StatsCombiner.class);
+        jobweek.setMapOutputKeyClass(Text.class);
+        jobweek.setMapOutputValueClass(GameSummary.class);
+        jobweek.setReducerClass(StatsReducer.class);
+        jobweek.setOutputKeyClass(Text.class);
+        jobweek.setOutputValueClass(NullWritable.class);
+        jobweek.setOutputFormatClass(TextOutputFormat.class);
+        jobweek.setInputFormatClass(SequenceFileInputFormat.class);
+        FileInputFormat.addInputPath(jobweek, new Path(args[0]));
+        FileOutputFormat.setOutputPath(jobweek, new Path(args[2]));
+
+        Configuration confmonth = new Configuration();
+        Job jobmonth = Job.getInstance(confmonth, "SecondJobMonth");
+        jobmonth.setNumReduceTasks(1);
+        jobmonth.setJarByClass(SecondJob.class);
+        jobmonth.setMapperClass(StatsMapperMonth.class);
+        jobmonth.setCombinerClass(StatsCombiner.class);
+        jobmonth.setMapOutputKeyClass(Text.class);
+        jobmonth.setMapOutputValueClass(GameSummary.class);
+        jobmonth.setReducerClass(StatsReducer.class);
+        jobmonth.setOutputKeyClass(Text.class);
+        jobmonth.setOutputValueClass(NullWritable.class);
+        jobmonth.setOutputFormatClass(TextOutputFormat.class);
+        jobmonth.setInputFormatClass(SequenceFileInputFormat.class);
+        FileInputFormat.addInputPath(jobmonth, new Path(args[0]));
+        FileOutputFormat.setOutputPath(jobmonth, new Path(args[3]));
+
+        System.exit(
+                job2.waitForCompletion(true) && jobmonth.waitForCompletion(true)
+                        && jobweek.waitForCompletion(true) ? 0 : 1);
     }
 }
